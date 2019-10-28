b4522be6cfa413ff59801bce1e81cee5e965810c
Anthony Do <47260573+anthonydo1@users.noreply.github.com>
2019-10-27 20:34:58 -0700

    Add files via upload

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,5 +1,19 @@
 package Do.cs146.project3;
 
 public class MazeSolver {
-
+	private Maze maze;
+	
+	public MazeSolver(Maze maze) {
+		this.maze = maze;
+	}
+	
+	
+	public void DFS() {
+		
+	}
+	
+	
+	public void BFS() {
+		
+	}
 }
