c9729ef0444220a25971e68956f858da208679ba
Anthony Do <47260573+anthonydo1@users.noreply.github.com>
2019-10-27 20:34:44 -0700

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,92 @@
+package Do.cs146.project3;
+
+import java.util.ArrayList;
+
+public class Cell {
+	private int row;
+	private int column;
+	private boolean north;
+	private boolean south;
+	private boolean east;
+	private boolean west;
+	private ArrayList<Cell> neighbors;
+	private boolean searched;
+	
+	public Cell(int row, int column) {
+		this.row = row;
+		this.column = column;
+		this.north = true;
+		this.south = true;
+		this.east = true;
+		this.west = true;
+		this.neighbors = new ArrayList<>();
+		this.searched = false;
+	}
+	
+
+	public void addNeighbor(Cell cell) {
+		neighbors.add(cell);
+	}
+	
+
+	public ArrayList<Cell> getNeighbors() {
+		return neighbors;
+	}
+	
+	public boolean hasBeenSearched() {
+		return searched;
+	}
+	
+	public boolean hasFullWalls() {
+		return north && south && east && west;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	
+	public void removeNorthWall() {
+		north = false;
+	}
+	
+	
+	public void removeSouthWall() {
+		south = false;
+	}
+	
+	
+	public void removeEastWall() {
+		east = false;
+	}
+	
+	
+	public void removeWestWall() {
+		west = false;
+	}
+	
+	
+	public boolean getNorthWall() {
+		return north;
+	}
+	
+	
+	public boolean getSouthWall() {
+		return south;
+	}
+	
+	
+	public boolean getEastWall() {
+		return east;
+	}
+	
+	
+	public boolean getWestWall() {
+		return west;
+	}
+}
---
+++ Maze.java
@@ -0,0 +1,159 @@
+package Do.cs146.project3;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+public class Maze {
+	
+	private int size;
+	private Cell[][] maze;
+	private Random random;
+	
+	public Maze(int size) {
+		this.size = size;
+		maze = new Cell[size][size];
+		random = new Random();
+	}
+	
+	public void generateCells() {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				maze[i][j] = new Cell(i, j);
+			}
+		}
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				Cell cell = maze[i][j];
+				
+				boolean northCell = (i + 1 >= 0) && (i + 1 < size) && (j >= 0) && (j < size);
+				boolean southCell = (i - 1 >= 0) && (i - 1 < size) && (j >= 0) && (j < size);
+				boolean eastCell = (i >= 0) && (i < size) && (j + 1 >= 0) && (j + 1 < size);
+				boolean westCell = (i >= 0) && (i < size) && (j - 1 >= 0) && (j - 1 < size);
+				
+				if (northCell) cell.addNeighbor(maze[i+1][j]);
+				if (southCell) cell.addNeighbor(maze[i-1][j]);
+				if (eastCell) cell.addNeighbor(maze[i][j+1]);
+				if (westCell) cell.addNeighbor(maze[i][j-1]);
+			}
+		}
+	}
+	
+	public void generateMaze() {
+		Stack<Cell> cellStack = new Stack<>();
+		int totalCells = size * size;
+		Cell currentCell = maze[0][0];
+		int visitedCells = 1;
+		
+		
+		while (visitedCells < totalCells) {
+			ArrayList<Cell> neighbors = currentCell.getNeighbors();
+			ArrayList<Cell> cells = new ArrayList<>();
+			
+			for (int i = 0; i < neighbors.size(); i++) {
+				if (neighbors.get(i).hasFullWalls()) {
+					cells.add(neighbors.get(i));
+				}
+			}
+			
+			if (cells.size() > 0) {
+				Cell cell = cells.get(random.nextInt(cells.size()));
+				removeAdjacentWall(cell, currentCell);
+				cellStack.push(cell);
+				currentCell = cell;
+				visitedCells++;
+			} else {
+				currentCell = cellStack.pop();
+			}
+		}
+	}
+	
+	
+	public static void removeAdjacentWall(Cell cell, Cell currentCell) {
+		int cellRow = cell.getRow();
+		int cellColumn = cell.getColumn();
+		
+		int currentCellRow = currentCell.getRow();
+		int currentCellColumn = currentCell.getColumn();
+		
+		if (cellRow - currentCellRow == 1) { 
+			cell.removeNorthWall();
+			currentCell.removeSouthWall();
+		} else if (cellRow - currentCellRow == -1) {
+			cell.removeSouthWall();
+			currentCell.removeNorthWall();
+		} else if (cellColumn - currentCellColumn == 1) {
+			cell.removeWestWall();
+			currentCell.removeEastWall();
+		} else if (cellColumn - currentCellColumn == -1) {
+			cell.removeEastWall();
+			currentCell.removeWestWall();
+		}
+	}
+	
+	
+	
+	public void printMaze() {
+		maze[0][0].removeNorthWall();
+		maze[size - 1][size - 1].removeSouthWall();
+		/*
+		for (int i = 0; i < size; i++) {
+			System.out.println("\n");
+			for (int j = 0; j < size; j++) {
+				Cell cell = maze[i][j];
+				System.out.print("\n");
+				System.out.print(" North: " + cell.getNorthWall());
+				System.out.print(" South: " + cell.getSouthWall());
+				System.out.print(" East: " + cell.getEastWall());
+				System.out.print(" West: " + cell.getWestWall());
+			}
+		}
+		*/
+		
+		for (int i = 0; i < size; i++) {
+			System.out.print("\n");
+			for (int j = 0; j < size; j++) {
+				Cell cell = maze[i][j];
+				if (cell.getNorthWall() == true) {
+					System.out.print("+-");
+				} else if (cell.getNorthWall() == false) {
+					System.out.print("+ ");
+				}
+			}
+			
+			System.out.print("+");
+			System.out.print("\n");
+			System.out.print("|");
+			
+			for (int j = 0; j < size; j++) {
+				Cell cell = maze[i][j];
+				if (cell.getEastWall() == true) {
+					System.out.print(" |");
+				} else if (cell.getEastWall() == false) {
+					System.out.print("  ");
+				}
+			}
+		}
+		
+		System.out.print("\n");
+		System.out.print("+");
+		
+		for (int j = 0; j < size; j++) {
+			Cell cell = maze[size-1][j];
+			if (cell.getSouthWall() == true) {
+				System.out.print("-+");
+			} else {
+				System.out.print(" +");
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Maze maze1 = new Maze(10);
+		maze1.generateCells();
+		maze1.generateMaze();
+		maze1.printMaze();
+	}
+}
